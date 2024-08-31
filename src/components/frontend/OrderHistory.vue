8f7a65cd512b2fc3c25f5765ea870bcf16daacc0
PoJui Chen <a0970872656@gmail.com>
2024-09-01 05:05:11 +0800

    update orderhistory mobile display

--- src/components/frontend/OrderHistory.vue
+++ src/components/frontend/OrderHistory.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card class="table-order">
     <v-data-table
       :headers="headers"
       :items="filteredOrders"
@@ -28,7 +28,7 @@
       </template>
       <template #item.create_at="{ item }">{{ $filters.date(item.create_at) }}</template>
       <template #item.orderId="{ item }">
-        <a @click.prevent="viewOrder(item)" class="order-Id">{{ item.orderId }}</a>
+        <a @click.prevent="viewOrder(item)" class="order-id">{{ item.orderId }}</a>
       </template>
       <template #item.cartTotal="{ item }">{{ $filters.currency(item.cartTotal) }}</template>
       <template #item.is_paid="{ item }">
@@ -36,6 +36,54 @@
         <span v-else class="text-danger">未付款</span>
       </template>
     </v-data-table>
+  </v-card>
+  <v-card class="table-list mt-3">
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          class="mt-3 mx-4 mb-2"
+          v-model="search"
+          @input="onClick"
+          :loading="loading"
+          append-inner-icon="mdi-magnify"
+          density="compact"
+          label="搜尋訂單編號"
+          placeholder="輸入訂單編號"
+          variant="solo"
+          hide-details
+          single-line
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-card class="list-header ma-2" style="max-height: 200px; overflow-y: scroll">
+      <v-card-text>
+        <v-row no-gutters>
+          <v-col :cols="dateCol">建立日期</v-col>
+          <v-col :cols="orderCol" class="text-left">訂單編號</v-col>
+          <v-col v-show="!isSmallScreen" cols="2" class="text-left">付款狀態</v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-virtual-scroll :items="filteredOrders" height="320" item-height="48">
+      <template v-slot:default="{ item }">
+        <v-card class="ma-2">
+          <v-card-text>
+            <v-row no-gutters>
+              <v-col :cols="dateCol">
+                {{ $filters.date(item.create_at) }}
+              </v-col>
+              <v-col :cols="orderCol" class="text-left">
+                <a @click.prevent="viewOrder(item)" class="order-id">{{ item.orderId }}</a>
+              </v-col>
+              <v-col v-show="!isSmallScreen" cols="2" class="text-left">
+                <span v-if="item.is_paid === true" class="text-success">已付款</span>
+                <span v-else class="text-danger">未付款</span>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </template>
+    </v-virtual-scroll>
   </v-card>
 </template>
 
@@ -63,6 +111,7 @@ export default {
       ],
       orders: [],
       localOrdersInfo: this.ordersInfo,
+      isSmallScreen: false,
     };
   },
   watch: {
@@ -91,6 +140,11 @@ export default {
       this.orders.push(orderstemp);
       this.orders.sort((a, b) => b.create_at - a.create_at);
     });
+    this.checkScreenSize();
+    window.addEventListener("resize", this.checkScreenSize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.checkScreenSize);
   },
   computed: {
     filteredOrders() {
@@ -98,6 +152,12 @@ export default {
         return this.orders;
       }
       return this.orders.filter((order) => order.orderId.includes(this.search));
+    },
+    dateCol() {
+      return this.isSmallScreen ? 4 : 3;
+    },
+    orderCol() {
+      return this.isSmallScreen ? 8 : 7;
     },
   },
   methods: {
@@ -112,12 +172,15 @@ export default {
       // 處理查看訂單的邏輯
       this.$router.push(`/checkout/${item.orderId}`);
     },
+    checkScreenSize() {
+      this.isSmallScreen = window.innerWidth <= 500;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.order-Id {
+.order-id {
   color: #1976d2;
   cursor: pointer;
   &:hover {
@@ -126,5 +189,11 @@ export default {
 }
 :deep(.v-data-table-footer__items-per-page) {
   display: none;
+}
+/* 隱藏滾動條但保留空間 */
+.v-card {
+  .list-header {
+    scrollbar-color: transparent transparent; /* 設置滾動條顏色為透明 */
+  }
 }
 </style>
